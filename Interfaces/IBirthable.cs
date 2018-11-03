7db07c1d674bfb45b464ff64c9cd73b4fc43bcbe
TheBest15 <itodorov91@gmail.com>
2018-11-03 06:17:45 +0200

    Add files via upload

---
+++ Interfaces/IBirthable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo.Interfaces
+{
+    interface IBirthable : IIdentifiable
+    {
+        string Birthdate { get;}
+    }
+}
---
+++ Interfaces/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo.Interfaces
+{
+    interface IPerson : IBirthable
+    {
+         string Name { get;}
+         int Age { get;}
+
+    }
+}
